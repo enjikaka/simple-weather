4ee042484426242b63ea9b2073fe4734cbc2f1d3
Jeremy Karlsson <karlsson@jeremy.se>
2016-04-10 00:06:09 +0200

    Added button to get location via Geolocation API

--- settings.js
+++ settings.js
@@ -1,20 +1,45 @@
 /* globals updateColor, updateBadge, $ */
 
+const dqs = (q) => {
+  return document.querySelector(q);
+};
+
+function fillOutLocation (value) {
+  window.localStorage.weatherLocation = value;
+  dqs('#my_loc').value = value;
+  updateMap();
+}
+
+function updateMap () {
+  dqs('#my_map').src = 'http://maps.googleapis.com/maps/api/staticmap?center=' + window.localStorage.weatherLocation + '&zoom=11&size=409x175&maptype=roadmap&sensor=false';
+}
+
+dqs('#find-me-button').addEventListener('click', function () {
+  if ('geolocation' in navigator) {
+    navigator.geolocation.getCurrentPosition(function (position) {
+      fillOutLocation(`${position.coords.latitude},${position.coords.longitude}`);
+    });
+  } else {
+    window.alert(`We can't find you, sorry.`);
+  }
+});
+
 window.onload = function () {
-  $('#my_loc').val(window.localStorage.weatherLocation);
-  $('#my_temp').val(window.localStorage.weatherTempVer);
+  $('#my_loc').val(window.localStorage.weatherLocation || '');
+  $('#my_temp').val(window.localStorage.weatherTempVer || 'c');
+  $('#my_theme').val(window.localStorage.weatherTheme || '');
+
   updateColor();
-  $('#my_theme').val(window.localStorage.weatherTheme);
-  $('#my_map').attr('src', 'http://maps.googleapis.com/maps/api/staticmap?center=' + window.localStorage.weatherLocation + '&zoom=11&size=409x175&maptype=roadmap&sensor=false');
+  updateMap();
   updateBadge();
 };
 
 window.onclick = function (e) {
   if (e.target.id === 'save') {
     window.localStorage.weatherLocation = $('#my_loc').val();
-    window.localStorage.weatherTempVer = $('#my_temp').val();
+    window.localStorage.weatherTempVer = $('#my_temp').val() || 'c';
     window.localStorage.weatherTheme = $('#my_theme').val();
-    $('#my_map').attr('src', 'http://maps.googleapis.com/maps/api/staticmap?center=' + window.localStorage.weatherLocation + '&zoom=11&size=409x175&maptype=roadmap&sensor=false');
+    updateMap();
     /* var notification = webkitNotifications.createNotification('http://apps.enji.se/weather/48.png', 'Woho!', 'Your settings are saved.');
     notification.show(); */
     updateBadge();
